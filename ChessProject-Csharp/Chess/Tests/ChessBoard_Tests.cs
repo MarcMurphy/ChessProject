387b55ff6d6dbc6ee552ddfd415f15b38dc9493e
Mark Murphy <contact@markmurphy.ie>
2017-12-30 19:09:31 +0000

    Whitespace changes (Tabs to spaces in tests, removing trailing empty lines)

--- ChessProject-Csharp/Chess/Tests/ChessBoard_Tests.cs
+++ ChessProject-Csharp/Chess/Tests/ChessBoard_Tests.cs
@@ -7,77 +7,77 @@ using System.Text;
 
 namespace Gfi.Hiring
 {
-	[TestFixture]
-	public class ChessBoard_Tests
-	{
-		private ChessBoard _chessBoard;
+    [TestFixture]
+    public class ChessBoard_Tests
+    {
+        private ChessBoard _chessBoard;
 
-		[SetUp]
-		public void SetUp()
-		{
-			_chessBoard = new ChessBoard();
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            _chessBoard = new ChessBoard();
+        }
 
-		[Test]
-		public void Has_MaxBoardWidth_of_7()
-		{
-			Assert.That(ChessBoard.MaxBoardWidth, Is.EqualTo(7));
-		}
+        [Test]
+        public void Has_MaxBoardWidth_of_7()
+        {
+            Assert.That(ChessBoard.MaxBoardWidth, Is.EqualTo(7));
+        }
 
-		[Test]
-		public void Has_MaxBoardHeight_of_7()
-		{
-			Assert.That(ChessBoard.MaxBoardHeight, Is.EqualTo(7));
-		}
+        [Test]
+        public void Has_MaxBoardHeight_of_7()
+        {
+            Assert.That(ChessBoard.MaxBoardHeight, Is.EqualTo(7));
+        }
 
-		[TestCase(0, 0)]
+        [TestCase(0, 0)]
         [TestCase(5, 5)]
         public void IsLegalBoardPosition_True_WhenCoordinateIsWithinBoardDimensions(int xCoordinate, int yCoordinate)
-		{
-			var isValidPosition = _chessBoard.IsLegalBoardPosition(xCoordinate, yCoordinate);
-			Assert.That(isValidPosition, Is.True);
-		}
+        {
+            var isValidPosition = _chessBoard.IsLegalBoardPosition(xCoordinate, yCoordinate);
+            Assert.That(isValidPosition, Is.True);
+        }
 
-		[TestCase(11, 5)]
+        [TestCase(11, 5)]
         [TestCase(0, 9)]
         [TestCase(11, 0)]
         [TestCase(-1, 5)]
         [TestCase(5, 1)]
         public void IsLegalBoardPosition_False_When_EitherCoordinateIsOutsideBoardDimensions(int xCoordinate, int yCoordinate)
-		{
-			var isValidPosition = _chessBoard.IsLegalBoardPosition(xCoordinate, yCoordinate);
-			Assert.That(isValidPosition, Is.False);
-		}
+        {
+            var isValidPosition = _chessBoard.IsLegalBoardPosition(xCoordinate, yCoordinate);
+            Assert.That(isValidPosition, Is.False);
+        }
 
-		[Test]
-		public void Avoids_Duplicate_Positioning()
-		{
-			Pawn firstPawn = new Pawn(PieceColor.Black);
-			Pawn secondPawn = new Pawn(PieceColor.Black);
-			_chessBoard.Add(firstPawn, 6, 3, PieceColor.Black);
-			_chessBoard.Add(secondPawn, 6, 3, PieceColor.Black);
+        [Test]
+        public void Avoids_Duplicate_Positioning()
+        {
+            Pawn firstPawn = new Pawn(PieceColor.Black);
+            Pawn secondPawn = new Pawn(PieceColor.Black);
+            _chessBoard.Add(firstPawn, 6, 3, PieceColor.Black);
+            _chessBoard.Add(secondPawn, 6, 3, PieceColor.Black);
             Assert.That(firstPawn.ChessBoard, Is.EqualTo(new Point(6, 3)));
             Assert.That(secondPawn.ChessBoard, Is.EqualTo(new Point(-1, -1)));
-		}
+        }
 
-		[Test]
-		public void Limits_The_Number_Of_Pawns()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				Pawn pawn = new Pawn(PieceColor.Black);
-				int row = i / ChessBoard.MaxBoardWidth;
-				_chessBoard.Add(pawn, 6 + row, i % ChessBoard.MaxBoardWidth, PieceColor.Black);
-				if (row < 1)
-				{
-					Assert.That(pawn.Coordinate.X, Is.EqualTo(6 + row));
-					Assert.That(pawn.Coordinate.Y, Is.EqualTo(i % ChessBoard.MaxBoardWidth));
-				}
-				else
-				{
-					Assert.That(pawn.Coordinate, Is.EqualTo(new Point(-1,-1)));
-				}
-			}
-		}
-	}
+        [Test]
+        public void Limits_The_Number_Of_Pawns()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Pawn pawn = new Pawn(PieceColor.Black);
+                int row = i / ChessBoard.MaxBoardWidth;
+                _chessBoard.Add(pawn, 6 + row, i % ChessBoard.MaxBoardWidth, PieceColor.Black);
+                if (row < 1)
+                {
+                    Assert.That(pawn.Coordinate.X, Is.EqualTo(6 + row));
+                    Assert.That(pawn.Coordinate.Y, Is.EqualTo(i % ChessBoard.MaxBoardWidth));
+                }
+                else
+                {
+                    Assert.That(pawn.Coordinate, Is.EqualTo(new Point(-1,-1)));
+                }
+            }
+        }
+    }
 }
--- ChessProject-Csharp/Chess/Tests/Pawn_Tests.cs
+++ ChessProject-Csharp/Chess/Tests/Pawn_Tests.cs
@@ -7,49 +7,48 @@ using System.Text;
 
 namespace Gfi.Hiring
 {
-	[TestFixture]
-	public class Pawn_Tests
-	{
-		private ChessBoard _chessBoard;
-		private Pawn _pawn;
+    [TestFixture]
+    public class Pawn_Tests
+    {
+        private ChessBoard _chessBoard;
+        private Pawn _pawn;
 
-		[SetUp]
-		public void SetUp()
-		{
-			_chessBoard = new ChessBoard();
-			_pawn = new Pawn(PieceColor.Black);
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            _chessBoard = new ChessBoard();
+            _pawn = new Pawn(PieceColor.Black);
+        }
 
-		[Test]
-		public void ChessBoard_Add_Sets_Coordinate()
-		{
-			_chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
-			Assert.That(_pawn.Coordinate, Is.EqualTo(new Point(6,3)));
-		}
+        [Test]
+        public void ChessBoard_Add_Sets_Coordinate()
+        {
+            _chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
+            Assert.That(_pawn.Coordinate, Is.EqualTo(new Point(6,3)));
+        }
 
-		[Test]
-		public void Pawn_Move_IllegalCoordinates_Right_DoesNotMove()
-		{
-			_chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
-			_pawn.Move(MovementType.Move, 7, 3);
+        [Test]
+        public void Pawn_Move_IllegalCoordinates_Right_DoesNotMove()
+        {
+            _chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
+            _pawn.Move(MovementType.Move, 7, 3);
             Assert.That(_pawn.Coordinate, Is.EqualTo(new Point(6, 3)));
         }
 
-		[Test]
-		public void Pawn_Move_IllegalCoordinates_Left_DoesNotMove()
-		{
-			_chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
-			_pawn.Move(MovementType.Move, 4, 3);
+        [Test]
+        public void Pawn_Move_IllegalCoordinates_Left_DoesNotMove()
+        {
+            _chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
+            _pawn.Move(MovementType.Move, 4, 3);
             Assert.That(_pawn.Coordinate, Is.EqualTo(new Point(6, 3)));
         }
 
-		[Test]
-		public void Pawn_Move_LegalCoordinates_Forward_UpdatesCoordinates()
-		{
-			_chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
-			_pawn.Move(MovementType.Move, 6, 2);
+        [Test]
+        public void Pawn_Move_LegalCoordinates_Forward_UpdatesCoordinates()
+        {
+            _chessBoard.Add(_pawn, 6, 3, PieceColor.Black);
+            _pawn.Move(MovementType.Move, 6, 2);
             Assert.That(_pawn.Coordinate, Is.EqualTo(new Point(6, 2)));
         }
-
-	}
+    }
 }
