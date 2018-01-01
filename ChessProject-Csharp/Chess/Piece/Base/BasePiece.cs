16b9303ee6fb91c3822cd944be92c46b75d9025c
Mark Murphy <contact@markmurphy.ie>
2018-01-01 20:53:46 +0000

    BasePiece should use a HashSet<Point> instead of a List<Point> as a .contains() on HashSet is faster when using a HashSet

--- ChessProject-Csharp/Chess/Piece/Base/BasePiece.cs
+++ ChessProject-Csharp/Chess/Piece/Base/BasePiece.cs
@@ -13,7 +13,7 @@ namespace Gfi.Hiring
 
         public Point Coordinate { get; set; }
 
-        public List<Point> ValidMovements { get; set; }
+        public HashSet<Point> ValidMovements { get; set; }
 
         public PieceType PieceType { get; set; }
 
